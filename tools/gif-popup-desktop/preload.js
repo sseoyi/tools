8cc86b974d9db27a41e995f9530648cff4a4ea99
sseoyi <seojoon1999@gmail.com>
2026-07-29 12:23:54 +0700

    Delete tools/gif-popup-desktop directory

--- tools/gif-popup-desktop/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  showPopup: (data) => ipcRenderer.send('show-popup', data),
-  stopPopup: () => ipcRenderer.send('stop-popup'),
-  resizePopup: (size) => ipcRenderer.send('resize-popup', size),
-  onPopupData: (callback) => ipcRenderer.on('popup-data', (event, data) => callback(data)),
-});
